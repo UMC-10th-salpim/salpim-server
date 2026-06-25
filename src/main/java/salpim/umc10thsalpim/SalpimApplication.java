c3b921c0742154671625779ffb9fa40c6361f82d
ChoiMinSang <min393917@gmail.com>
2026-06-25 22:52:36 +0900

    initial commit

---
+++ src/main/java/salpim/umc10thsalpim/SalpimApplication.java
@@ -0,0 +1,13 @@
+package salpim.umc10thsalpim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalpimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalpimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/salpim/umc10thsalpim/SalpimApplicationTests.java
@@ -0,0 +1,13 @@
+package salpim.umc10thsalpim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalpimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
