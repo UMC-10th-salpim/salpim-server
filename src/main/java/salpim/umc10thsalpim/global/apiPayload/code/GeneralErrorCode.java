61a857d5202288769c3ae598a5f7b9c3199745d9
migi0401 <migi111@naver.com>
2026-06-28 22:19:05 +0900

    feat: GeneralSuccessCode 추가

--- src/main/java/salpim/umc10thsalpim/global/apiPayload/code/GeneralErrorCode.java
+++ src/main/java/salpim/umc10thsalpim/global/apiPayload/code/GeneralErrorCode.java
@@ -14,6 +14,7 @@ public enum GeneralErrorCode implements BaseErrorCode {
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증되지 않았습니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "접근이 금지되었습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "COMMON404", "해당 리소스를 찾을 수 없습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 오류입니다.");
     ;
 
     private final HttpStatus status;
