c975d4c517456e571c6d782d22e7f9cb79834d6d
migi0401 <migi111@naver.com>
2026-06-28 22:19:43 +0900

    feat: 응답 통일 & 에러 핸들링

---
+++ src/main/java/salpim/umc10thsalpim/global/apiPayload/hander/GeneralExceptionAdvice.java
@@ -0,0 +1,53 @@
+package salpim.umc10thsalpim.global.apiPayload.hander;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import salpim.umc10thsalpim.global.apiPayload.ApiResponse;
+import salpim.umc10thsalpim.global.apiPayload.code.BaseErrorCode;
+import salpim.umc10thsalpim.global.apiPayload.code.GeneralErrorCode;
+import salpim.umc10thsalpim.global.apiPayload.exception.ProjectException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GeneralExceptionAdvice {
+
+    // 프로젝트에서 발생한 예외 처리
+    @ExceptionHandler(ProjectException.class)
+    public ResponseEntity<ApiResponse<Void>> handleMemberException(
+            ProjectException e
+    ) {
+        BaseErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(ApiResponse.onFailure(errorCode, null));
+    }
+
+    // 그 외의 정의되지 않은 모든 예외 처리 -> 서버 오류로 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<String>> handleException(
+            Exception ex
+    ) {
+        BaseErrorCode code = GeneralErrorCode.INTERNAL_SERVER_ERROR;
+        return ResponseEntity.status(code.getStatus())
+                .body(ApiResponse.onFailure(code, ex.getMessage()));
+    }
+
+    // @Valid 어노테이션 검증 실패 예외
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e
+    ){
+        // 검증 실패한 변수명과 실패 이유를 담을 Map
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(
+                error -> {errors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        BaseErrorCode code = GeneralErrorCode.BAD_REQUEST;
+
+        return ResponseEntity.status(code.getStatus())
+                .body(ApiResponse.onFailure(code, errors));
+    }
+}
