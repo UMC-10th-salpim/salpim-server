a9fc029b75c9609f72f51d7059b376601de40cc1
2u6in <zxcv35602667@gmail.com>
2026-07-08 10:20:40 +0900

    feat: recommedation 도메인 엔티티 추가

---
+++ src/main/java/salpim/umc10thsalpim/domain/recommendation/entity/RecommendationOption.java
@@ -0,0 +1,35 @@
+package salpim.umc10thsalpim.domain.recommendation.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import salpim.umc10thsalpim.domain.welfare.entity.WelfareCategory;
+import salpim.umc10thsalpim.global.entity.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "recommendation_option")
+public class RecommendationOption extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "option_text", nullable = false)
+    private String optionText;
+
+    @Column(name = "option_order", nullable = false)
+    private Integer optionOrder;
+
+    @Column(name = "search_key")
+    private String searchKey;
+
+    @JoinColumn(name = "category_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private WelfareCategory welfareCategory;
+}
