4417f26530e4f929a884226bfd4640c78f000a71
andyoon <andyoon502@naver.com>
2026-07-17 04:00:46 +0900

    feat: 회원, 지역 도메인 최소 기반 추가

---
+++ src/main/java/salpim/umc10thsalpim/domain/member/enums/Gender.java
@@ -0,0 +1,7 @@
+package salpim.umc10thsalpim.domain.member.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    NONE
+}
---
+++ src/main/java/salpim/umc10thsalpim/domain/region/enums/RegionLevel.java
@@ -0,0 +1,7 @@
+package salpim.umc10thsalpim.domain.region.enums;
+
+public enum RegionLevel {
+    SIDO,
+    SIGUNGU,
+    DONG
+}
