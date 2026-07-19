1ebed0776ebc796d477245f27dc51b3fcca602d7
ChoiMinSang <min393917@gmail.com>
2026-07-20 04:28:43 +0900

    feat: ãrestclient, xmlMapper bean 추가

---
+++ src/main/java/salpim/umc10thsalpim/global/config/WelfareApiConfig.java
@@ -0,0 +1,22 @@
+package salpim.umc10thsalpim.global.config;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class WelfareApiConfig {
+
+   // 복지서비스(지자체/중앙) api용 RestClient
+   // baseUrl은 각 서비스에서 application.yml 값을 주입받음
+   @Bean
+   public RestClient welfareRestClient(){
+      return RestClient.builder().build();
+   }
+
+   @Bean
+   public XmlMapper xmlMapper(){
+      return new XmlMapper();
+   }
+}
