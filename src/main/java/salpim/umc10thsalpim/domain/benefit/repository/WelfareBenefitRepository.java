c6f40f6a716ee2da4a96f5c19b8de5b558a20be2
2u6in <zxcv35602667@gmail.com>
2026-07-22 11:23:17 +0900

    refactor: @EntityGraph 어노테이션 추가하여 N+1 문제 발생 해결

--- src/main/java/salpim/umc10thsalpim/domain/benefit/repository/WelfareBenefitRepository.java
+++ src/main/java/salpim/umc10thsalpim/domain/benefit/repository/WelfareBenefitRepository.java
@@ -1,10 +1,12 @@
 package salpim.umc10thsalpim.domain.benefit.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import salpim.umc10thsalpim.domain.benefit.entity.WelfareBenefit;
 
 import java.util.List;
 
 public interface WelfareBenefitRepository extends JpaRepository<WelfareBenefit, Long> {
+    @EntityGraph(attributePaths = "welfareCategory")
     List<WelfareBenefit> findByExternalIdInAndSource(List<String> externalIdList, String source);
 }
