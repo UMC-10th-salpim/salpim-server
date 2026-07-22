0170212bd4a31a003e3233a669c121846a63ac6f
2u6in <zxcv35602667@gmail.com>
2026-07-22 12:18:16 +0900

    fix: @Positive 검증 추가

--- src/main/java/salpim/umc10thsalpim/domain/benefit/controller/BenefitController.java
+++ src/main/java/salpim/umc10thsalpim/domain/benefit/controller/BenefitController.java
@@ -1,5 +1,6 @@
 package salpim.umc10thsalpim.domain.benefit.controller;
 
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,11 +27,12 @@ public class BenefitController {
     @GetMapping("/v1/welfare-benefits/search")
     public ApiResponse<CursorResDTO.Pagination<BenefitResDTO.WelfareSearchResultDTO>> getSearchResult
     (
-            @RequestParam(required = false, defaultValue = "") String searchKey,
+            @RequestParam(required = false, defaultValue = "")
+            String searchKey,
             @RequestParam List<Long> regionIds, //필수
             @RequestParam(required = false) List<Long> categoryIds, //null -> 모든 카테고리
             @RequestParam(name="cursor", defaultValue = "-1") String cursor,
-            @RequestParam(name="pageSize", defaultValue = "10") Integer pageSize,
+            @RequestParam(name="pageSize", defaultValue = "10") @Positive Integer pageSize,
             @RequestParam(name="sort", defaultValue = "popular") String sort
     ){
         return ApiResponse.onSuccess(BenefitSuccessCode.BENEFIT_LIST_GET_SUCCESS, benefitService.getSearchResult(searchKey, regionIds, categoryIds, cursor, pageSize, sort));
