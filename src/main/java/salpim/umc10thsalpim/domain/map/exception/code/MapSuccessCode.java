27b3221ff11d0a86944a9ec03fb11e583c2aaf5f
migi0401 <migi111@naver.com>
2026-07-23 22:35:57 +0900

    feat: 지도 상세 조회 성공 & 에러 코드 추가

--- src/main/java/salpim/umc10thsalpim/domain/map/exception/code/MapSuccessCode.java
+++ src/main/java/salpim/umc10thsalpim/domain/map/exception/code/MapSuccessCode.java
@@ -1,21 +1,19 @@
 package salpim.umc10thsalpim.domain.map.exception.code;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import salpim.umc10thsalpim.global.apiPayload.code.BaseSuccessCode;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public enum MapSuccessCode implements BaseSuccessCode {
 
-    OK(HttpStatus.OK,
-            "COMMON200",
-            "시설 정보를 조회했습니다.");
+    FACILITY_INFO_SUCCESS(HttpStatus.OK, "MAP2000", "시설 상세 정보 및 복지 혜택을 성공적으로 조회했습니다."),
+    NEARBY_FACILITY_SEARCH_SUCCESS(HttpStatus.OK, "MAP2001", "주변 시설 목록을 성공적으로 조회했습니다."),
+    MAP_SEARCH_SUCCESS(HttpStatus.OK, "MAP2002", "지도 검색 결과를 성공적으로 조회했습니다.");
 
     private final HttpStatus status;
     private final String code;
     private final String message;
-
 }
