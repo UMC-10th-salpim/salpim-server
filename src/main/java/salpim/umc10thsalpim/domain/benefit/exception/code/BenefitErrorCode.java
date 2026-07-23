2094675b7a0b61f977e5849c58cc1c627998f4b2
andyoon <andyoon502@naver.com>
2026-07-23 17:49:23 +0900

    fix: 지역 범위 오류메세지 중복 제거

--- src/main/java/salpim/umc10thsalpim/domain/benefit/exception/code/BenefitErrorCode.java
+++ src/main/java/salpim/umc10thsalpim/domain/benefit/exception/code/BenefitErrorCode.java
@@ -21,7 +21,6 @@ public enum BenefitErrorCode implements BaseErrorCode {
     BENEFIT_REGION_LEVEL_MISMATCH(HttpStatus.INTERNAL_SERVER_ERROR,
             "BENEFIT500_2",
             "혜택의 지역이 지역 범위와 일치하지 않습니다."),
-            "혜택의 지역이 신청 규칙의 지역 범위와 일치하지 않습니다."),
     INVALID_SORT_TYPE(HttpStatus.BAD_REQUEST,
             "BENEFIT400",
             "유효하지 않은 정렬 방식입니다."),
