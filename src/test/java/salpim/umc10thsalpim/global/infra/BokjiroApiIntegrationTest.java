7219be7372e65d8a1468d48a6f16638d59a5b06d
2u6in <zxcv35602667@gmail.com>
2026-08-09 22:29:56 +0900

    test: 테스트 코드 수정된 코드에 맞게 수정

--- src/test/java/salpim/umc10thsalpim/global/infra/BokjiroApiIntegrationTest.java
+++ src/test/java/salpim/umc10thsalpim/global/infra/BokjiroApiIntegrationTest.java
@@ -1,16 +1,17 @@
 package salpim.umc10thsalpim.global.infra;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import salpim.umc10thsalpim.global.infra.bokjiro.BokjiroApiClient;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Disabled("외부 API 및 환경 변수 필요")
+//@Disabled("외부 API 및 환경 변수 필요")
 public class BokjiroApiIntegrationTest {
 
     @Autowired
@@ -19,8 +20,8 @@ public class BokjiroApiIntegrationTest {
     @Test
     @DisplayName("중앙부처 복지로 api 연결 확인")
     void nationalBokjiroApiTest(){
-        var res = bokjiroApiClient.searchNationalBenefits(1, 10, null, null);
-        assertThat(res.getTotalCount()).isPositive();
+        var res = bokjiroApiClient.searchBenefits(1, 10, List.of(""), null, "National", null, null);
+        assertThat(res.getMaxTotalCount()).isPositive();
         assertThat(res.getBenefitList()).isNotEmpty();
         assertThat(res.getBenefitList().get(0).getServId()).isNotBlank();
     }
@@ -28,8 +29,8 @@ public class BokjiroApiIntegrationTest {
     @Test
     @DisplayName("지자체 복지로 api 연결 확인")
     void localBokjiroApiTest(){
-        var res = bokjiroApiClient.searchLocalBenefits(1, 10, null, null, "인천", null);
-        assertThat(res.getTotalCount()).isPositive();
+        var res = bokjiroApiClient.searchBenefits(1, 10, List.of(""), null, "Local", "인천", null);
+        assertThat(res.getMaxTotalCount()).isPositive();
         assertThat(res.getBenefitList()).isNotEmpty();
         assertThat(res.getBenefitList().get(0).getServId()).isNotBlank();
     }
