c17de0360e6d3c9044e4704ac62b40c08cdbc0f2
ChoiMinSang <min393917@gmail.com>
2026-08-10 20:46:43 +0900

    feat: 사용자 행정복지센터 제공 api 구현

--- src/main/java/salpim/umc10thsalpim/domain/member/exception/code/MemberSuccessCode.java
+++ src/main/java/salpim/umc10thsalpim/domain/member/exception/code/MemberSuccessCode.java
@@ -21,6 +21,9 @@ public enum MemberSuccessCode implements BaseSuccessCode {
     MEMBER_PASSWORD_CHANGED(HttpStatus.OK,
             "MEMBER200_4",
             "비밀번호를 성공적으로 변경했습니다."),
+    MEMBER_WELFARE_CENTER_VIEW(HttpStatus.OK,
+            "MEMBER200_5",
+            "복지관 정보를 성공적으로 조회했습니다."),
     ;
 
     private final HttpStatus status;
